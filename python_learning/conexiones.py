2781244afde3162c0920aaab2c326d7de1bd51e7
DIDIER ACUÑA <162734143+TheLastDBA@users.noreply.github.com>
2025-10-28 16:15:05 -0500

    Modify database connection in conexiones.py
    Updated database connection parameters to empty values.

--- python_learning/conexiones.py
+++ python_learning/conexiones.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import Error
 
 try:
-    conexion_tecnosoft = mysql.connector.connect(user = 'prueba',database='tecnosoft',host='10.1.2.89',port='3306',password='Tecnoglass*2025')
+    conexion_tecnosoft = mysql.connector.connect(user = 'prueba',database='',host='',port='3306',password='')
 
     print('Estado de conexion:' , conexion_tecnosoft)
 
